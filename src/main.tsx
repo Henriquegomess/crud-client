dd0372ae780fe7a5f9c322cdc33789f6fd57f440
Henrique Gomes <hgs@icomp.ufam.edu.br>
2024-07-08 23:32:28 -0400

    chore: update ESLint config and Storybook setup
    - Added new ESLint plugins and rules
    - Updated Storybook configuration for stories and addons
    - Removed package-lock.json file

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
+import { worker } from './mocks/browser';
 
 import App from './app/app';
+
+if (process.env.NODE_ENV === 'development') {
+  worker.start();
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
